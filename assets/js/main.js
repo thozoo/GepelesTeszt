7f36f09fc28d1ecbdf159fbbff7db2551d6d3ce7
Tóth Zoltán <thozoo@gmail.com>
2023-10-04 13:23:58 +0200

    Szinez is

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 var szovegek = [
-    "lorem ipsum dolor sit amet",
-    "sanyi és peti mindig ötös",
-    "tomi mosóport vettem tegnap",
-    "nincs kettő négy nélkül",
-    "ki mint veti ágyát úgy alussza álmát",
-    "minden majom telefonál naponta",
-    "a kockázatok és mellékhatások",
-    "csabi egy dobozfejű titán",
-    "Thozo the freaking GOAT",
-    "Aki skypeot használ az nem normális",
+  "lorem ipsum dolor sit amet",
+  "sanyi és peti mindig ötös",
+  "tomi mosóport vettem tegnap",
+  "nincs kettő négy nélkül",
+  "ki mint veti ágyát úgy alussza álmát",
+  "minden majom telefonál naponta",
+  "a kockázatok és mellékhatások",
+  "csabi egy dobozfejű titán",
+  "Thozo the freaking GOAT",
+  "Aki skypeot használ az nem normális",
 ];
 
-var rnd = Math.floor(Math.random()*szovegek.length);
+var rnd = Math.floor(Math.random() * szovegek.length);
 
 document.getElementById("szoveg").innerHTML = szovegek[rnd];
 
@@ -19,22 +19,31 @@ var ido = 0;
 var mehet = true;
 
 document.getElementById("bemenet").addEventListener("input", () => {
-if (mehet)
-{
+  if (mehet) {
     mehet = false;
-var idozito = setInterval(() => 
-{
-    ido++;
-    document.getElementById("ido").innerHTML = (ido/10)+" mp";
-    if (szovegek[rnd] == document.getElementById("bemenet").value)
-    {
+    var idozito = setInterval(() => {
+      ido++;
+      document.getElementById("ido").innerHTML = ido / 10 + " mp";
+      if (szovegek[rnd] == document.getElementById("bemenet").value) {
         clearInterval(idozito);
-        var cpm = Math.round(szovegek[rnd].length/(ido/10)*100)/100;
-        document.getElementById("cpm").innerHTML = cpm+" karakter / mp";
-
+        var cpm = Math.round((szovegek[rnd].length / (ido / 10)) * 100) / 100;
+        document.getElementById("cpm").innerHTML = cpm + " karakter / mp";
         document.getElementById("kesz").style.display = "block";
-    }
-}, 100);
-}
+      }
+    }, 100);
+  }
 });
 
+var szin = () => {
+  var beirt = document.getElementById("bemenet").value;
+  document.getElementById("szoveg").innerHTML = "";
+  for (var i = 0; i < szovegek[rnd].length; i++) {
+    if (szovegek[rnd][i] == beirt[i]) {
+      document.getElementById("szoveg").innerHTML +=
+        "<span style='color:green'>" + szovegek[rnd][i] + "</span>";
+    } else {
+      document.getElementById("szoveg").innerHTML +=
+        "<span style='color:red'>" + szovegek[rnd][i] + "</span>";
+    }
+  }
+};
